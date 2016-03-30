d397c999d0b11fce39c23657c018a290134849e6
Davida Gaffney <gdavida@gmail.com>
2016-03-30 12:41:38 -0500

    For Insect and insect_test Add ActiveRecord validations and replace with manual my_valid's and test to make sure they work.

--- lib/insect.rb
+++ lib/insect.rb
@@ -1,10 +1,16 @@
-# + name
-# + location
-# + description
-# + researcher_id
+# + name - string
+# + location - string
+# + description - string
+# + researcher_id - integer
 
 class Insect < ActiveRecord::Base
   
+	validates :name, presence: true
+	validates :location, presence: true
+	validates :description, presence: true
+	validates :researcher_id, presence: true, numericality: { only_integer: true }
+
+
   def researcher
     Researcher.find_by_id(researcher_id)
   end
